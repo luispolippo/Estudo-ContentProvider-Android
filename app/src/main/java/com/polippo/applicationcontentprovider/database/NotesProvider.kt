2355c76639760e02aeabd65a2b873f46de0aa3d5
Luis Guilherme Polippo <luispolippo@hotmail.com>
2021-03-10 14:33:50 -0300

    implemented the functions on NotesProvider

--- app/src/main/java/com/polippo/applicationcontentprovider/database/NotesProvider.kt
+++ app/src/main/java/com/polippo/applicationcontentprovider/database/NotesProvider.kt
@@ -2,49 +2,104 @@ package com.polippo.applicationcontentprovider.database
 
 import android.content.ContentProvider
 import android.content.ContentValues
+import android.content.Context
 import android.content.UriMatcher
 import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
+import android.media.UnsupportedSchemeException
 import android.net.Uri
+import android.os.Build
+import android.provider.BaseColumns._ID
+import androidx.annotation.RequiresApi
 
 class NotesProvider : ContentProvider() {
 
     private lateinit var mUriMatcher: UriMatcher
+    private lateinit var dbHelper: NotesDataBaseHelper
 
     override fun onCreate(): Boolean {
         mUriMatcher = UriMatcher(UriMatcher.NO_MATCH)
         mUriMatcher.addURI(AUTHORITY, "notes", NOTES)
         mUriMatcher.addURI(AUTHORITY, "notes/#", NOTES_BY_ID)
+
+        if(context != null){ dbHelper = NotesDataBaseHelper(context as Context) }
+
         return true
     }
 
+    @RequiresApi(Build.VERSION_CODES.JELLY_BEAN_MR2)
     override fun delete(uri: Uri, selection: String?, selectionArgs: Array<String>?): Int {
-        TODO("Implement this to handle requests to delete one or more rows")
+        if(mUriMatcher.match(uri) == NOTES_BY_ID){
+            val db: SQLiteDatabase = dbHelper.writableDatabase
+            val linesAffect = db.delete(NotesDataBaseHelper.TABLE_NOTES, "$_ID = ?", arrayOf(uri.lastPathSegment))
+            db.close()
+
+            context?.contentResolver?.notifyChange(uri, null)
+            return linesAffect
+        } else {
+            throw UnsupportedSchemeException("Uri inválida para exclusão!")
+        }
     }
 
-    override fun getType(uri: Uri): String? {
-        TODO(
-            "Implement this to handle requests for the MIME type of the data" +
-                    "at the given URI"
-        )
-    }
+    @RequiresApi(Build.VERSION_CODES.JELLY_BEAN_MR2)
+    override fun getType(uri: Uri): String? = throw UnsupportedSchemeException("Uri não implementada")
 
+    @RequiresApi(Build.VERSION_CODES.JELLY_BEAN_MR2)
     override fun insert(uri: Uri, values: ContentValues?): Uri? {
-        TODO("Implement this to handle requests to insert a new row.")
+        if(mUriMatcher.match(uri) == NOTES){
+            val db = dbHelper.writableDatabase
+            val id = db.insert(NotesDataBaseHelper.TABLE_NOTES, null, values)
+            val insertUri = Uri.withAppendedPath(BASE_URI, id.toString())
+            db.close()
+            context?.contentResolver?.notifyChange(uri, null)
+            return insertUri
+        } else{
+            throw UnsupportedSchemeException("Uri inválida para inserção")
+        }
     }
 
 
+    @RequiresApi(Build.VERSION_CODES.JELLY_BEAN_MR2)
     override fun query(
         uri: Uri, projection: Array<String>?, selection: String?,
         selectionArgs: Array<String>?, sortOrder: String?
     ): Cursor? {
-        TODO("Implement this to handle query requests from clients.")
+        return when{
+            mUriMatcher.match(uri) == NOTES -> {
+                val db = dbHelper.writableDatabase
+                val cursor =
+                        db.query(NotesDataBaseHelper.TABLE_NOTES, projection, selection, selectionArgs, null,null, sortOrder )
+                cursor.setNotificationUri(context?.contentResolver, uri)
+                cursor
+            }
+            mUriMatcher.match(uri) == NOTES_BY_ID -> {
+                val db = dbHelper.writableDatabase
+                val cursor =
+                        db.query(NotesDataBaseHelper.TABLE_NOTES, projection, "$_ID = ?", arrayOf(uri.lastPathSegment), null, null, sortOrder)
+                cursor.setNotificationUri(context?.contentResolver, uri)
+                cursor
+            }
+            else -> {
+                throw UnsupportedSchemeException("Uri não implementada")
+            }
+        }
     }
 
+    @RequiresApi(Build.VERSION_CODES.JELLY_BEAN_MR2)
     override fun update(
         uri: Uri, values: ContentValues?, selection: String?,
         selectionArgs: Array<String>?
     ): Int {
-        TODO("Implement this to handle requests to update one or more rows.")
+        if(mUriMatcher.match(uri) == NOTES_BY_ID){
+            val db = dbHelper.writableDatabase
+            val linesAffect =
+                    db.update(NotesDataBaseHelper.TABLE_NOTES, values, "$_ID = ?", arrayOf(uri.lastPathSegment))
+            db.close()
+            context?.contentResolver?.notifyChange(uri, null)
+            return linesAffect
+        } else {
+            throw UnsupportedSchemeException("Uri não implementada")
+        }
     }
 
     companion object{
